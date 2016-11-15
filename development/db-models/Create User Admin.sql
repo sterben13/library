fa48dca78ddb536c46f77a83e7ddf24cb42dadd1
sterben13 <ivan.dev.job@gmail.com>
2016-11-15 10:28:55 -0600

    Tareas Realizadas
    Creacion de un nuevo usuario adminsitrativo de la BD, Inicializacion de la aplicacion en entorno de desarrollador y configuracion de la BD

---
+++ development/db-models/Create User Admin.sql
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  rous
+ * Created: 15/11/2016
+ */
+CREATE USER 'Admin'@'localhost' IDENTIFIED BY 'Yiilibrary';
+GRANT ALL PRIVILEGES ON library.* TO 'Admin'@'localhost';
+FLUSH PRIVILEGES;
+
