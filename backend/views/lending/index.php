bc86fc29374289b9650a19cd56c5316d89882048
sterben13 <ivan.dev.job@gmail.com>
2016-12-06 23:30:03 -0600

    Prestamos
    Tareas realizadas:
    Modificación de la tabla lendings
    Select de nombre de usuarios
    select de títulos de libros solo mostrara los disponible
    crear un préstamo y modificas la disponibilidad del ejemplar
    Búsqueda solo por usuario
    Modificaciones de la vista

--- backend/views/lending/index.php
+++ backend/views/lending/index.php
@@ -2,35 +2,77 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use backend\models\User;
+use backend\models\Book;
+use \backend\models\Copy;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\LendingSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Lendings';
+$this->title = 'Préstamos';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="lending-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    
+    
 
     <p>
-        <?= Html::a('Create Lending', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('REGISTRAR PRÉSTAMO', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+    
+    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+        
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
-            'user_id',
+            [
+                'attribute' => 'user_id',
+                'value' => function ($model){
+                    $user = User::find()->where(['user_id'=>$model->user_id])->one();
+                    return $user['user_names']
+                            .' '.$user['user_lastname']
+                            .' '.$user['user_snd_lastname'];
+                },
+                'label' => "Nombre del Usuario"
+            ],
+            [
+                'attribute' => 'copy_id',
+                'value' => function ($model){
+                    $copy = Copy::find()->where(['copy_id'=>$model->copy_id])->one();
+                    $book = Book::find()->where(['book_id'=>$copy->book_id])->one();
+                    return $book->book_title;
+                },
+                'label' => 'Titulo del Ejemplar'
+            ],
             'copy_id',
-            'lend_auth_at',
-            'lend_return_at',
-            'lend_return_real',
-
+            [
+                'attribute'=>'lend_auth_at',
+                'value'=> function ($model){
+                    Yii::$app->formatter->locale = 'es-mx';
+                    return Yii::$app->formatter->asDate($model->lend_auth_at);
+                }
+            ],
+            [
+                'attribute'=>'lend_return_at',
+                'value'=> function ($model){
+                    Yii::$app->formatter->locale = 'es-mx';
+                    return Yii::$app->formatter->asDate($model->lend_return_at);
+                }
+            ],
+            [
+                'attribute'=>'lend_return_real',
+                'value'=> function ($model){
+                    Yii::$app->formatter->locale = 'es-mx';
+                    return Yii::$app->formatter->asDate($model->lend_return_real);
+                }
+            ],
             ['class' => 'yii\grid\ActionColumn'],
         ],
+        'options'=>[
+            'style'=>'background:white;'
+        ]
     ]); ?>
 </div>
